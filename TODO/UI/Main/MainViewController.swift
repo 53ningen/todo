1ee40e69570cf89fe54eea11c9d21da88cb9d4ec
gomi_ningen <korun24276@gmail.com>
2016-03-16 11:58:35 +0900

    Issueが表示されるようにした

--- TODO/UI/Main/MainViewController.swift
+++ TODO/UI/Main/MainViewController.swift
@@ -11,7 +11,17 @@ final class MainViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainTabBar.delegate = self
-        mainTabBar.selectedItem = issuesItem
+        tableView.registerNib(IssueCellView.self)
+        tableView.registerNib(LabelCellView.self)
+        tableView.delegate = self
+        tableView.dataSource = self
+        // tableView上下の余計なスペースを除去するために必要
+        tableView.contentInset = UIEdgeInsets(top: -1, left: 0, bottom: 0, right: 0)
+        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0.1))
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0.1))
+        if tableView.respondsToSelector("separatorInset") { tableView.separatorInset = UIEdgeInsetsZero }
+        if tableView.respondsToSelector("layoutMargins") { tableView.layoutMargins = UIEdgeInsetsZero }
+
     }
 
 }
@@ -20,6 +30,29 @@ extension MainViewController: UITabBarDelegate {
 
     func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
         NSLog("\(item.tag)")
+    }
+    
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 1 // table上部の余白を消すためにつける
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 75
+    }
+
+}
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier(IssueCellView.cellReuseIdentifier, forIndexPath: indexPath)
     }
     
 }
--- TODO/UI/UIUtils.swift
+++ TODO/UI/UIUtils.swift
@@ -1,11 +1,37 @@
 /// UI層で用いるユーティリティーメソッド
 
+import UIKit
 import Foundation
 
 extension NSObject {
     
+    static func getClassName() -> String {
+        return NSStringFromClass(self).componentsSeparatedByString(".").last! as String
+    }
+
     func getClassName() -> String {
         return NSStringFromClass(self.dynamicType).componentsSeparatedByString(".").last! as String
+    }
+    
+}
+
+
+extension UITableViewCell {
+    
+    static func getUINib() -> UINib {
+        return UINib(nibName: cellReuseIdentifier, bundle: NSBundle.mainBundle())
+    }
+    
+    static var cellReuseIdentifier: String {
+        return self.getClassName()
+    }
+    
+}
+
+extension UITableView {
+    
+    func registerNib(cls: UITableViewCell.Type) {
+        registerNib(cls.getUINib(), forCellReuseIdentifier: cls.cellReuseIdentifier)
     }
     
 }
