fd991905e13b60d19ae6318aa54ec39a756faddb
gomi_ningen <korun24276@gmail.com>
2016-03-16 10:54:03 +0900

    UI層のファイルを整理

--- TODO/UI/Main/MainViewController.swift
+++ TODO/UI/Main/MainViewController.swift
@@ -2,4 +2,24 @@ import UIKit
 
 final class MainViewController: BaseViewController {
 
+    @IBOutlet weak var mainTabBar: UITabBar!
+    @IBOutlet weak var issuesItem: UITabBarItem!
+    @IBOutlet weak var labelsItem: UITabBarItem!
+    @IBOutlet weak var milestonesItem: UITabBarItem!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTabBar.delegate = self
+        mainTabBar.selectedItem = issuesItem
+    }
+
+}
+
+extension MainViewController: UITabBarDelegate {
+
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        NSLog("\(item.tag)")
+    }
+    
 }
