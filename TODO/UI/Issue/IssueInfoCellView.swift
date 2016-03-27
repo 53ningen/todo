953438c68bf980d2501c57fbfc67931d07e702fc
gomi_ningen <korun24276@gmail.com>
2016-03-28 00:45:03 +0900

    Issueの説明文表示のデザインを変更

--- TODO/UI/Issue/IssueInfoCellView.swift
+++ TODO/UI/Issue/IssueInfoCellView.swift
@@ -5,12 +5,19 @@ final class IssueInfoCellView: UITableViewCell {
     
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func willMoveToSuperview(newSuperview: UIView?) {
+        super.willMoveToSuperview(newSuperview)
+        descriptionTextView.roundedCorners(5)
+        descriptionTextView.border(1, color: UIColor.borderColor.CGColor)
+    }
     
     func bind(issue: Issue) {
         idLabel.text = "#\(issue.id.value)"
         titleLabel.text = issue.info.title
-        descriptionLabel.text = issue.info.desc
+        descriptionTextView.text = issue.info.desc.isEmpty ? "No description provided." : issue.info.desc
+        descriptionTextView.textColor = issue.info.desc.isEmpty ? UIColor.lightGrayColor() : UIColor.blackColor()
     }
     
 }
