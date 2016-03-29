52638ebe245ccf8079f9928de8306079cb7cb5cb
gomi_ningen <korun24276@gmail.com>
2016-03-29 20:06:30 +0900

    Realmのスキーマバージョンを割り振った

--- TODO/DB/Realm/RealmCommon.swift
+++ TODO/DB/Realm/RealmCommon.swift
@@ -3,13 +3,28 @@ import RealmSwift
 /// unix time
 typealias RealmDate = Int64
 
+
+/// Realmの初期化などRealm固有の操作を閉じ込めておくI/F
 public struct RealmOperator {
     
-    static func deleteAll() {
+    public static func deleteAll() {
         let realm = try! Realm()
         try! realm.write {
             realm.deleteAll()
         }
     }
+    
+    public static func initialize() {
+        Realm.Configuration.defaultConfiguration = config
+    }
+    
+    private static let config = Realm.Configuration(
+        // Realm/Objectsを変更したら数字をあげる
+        schemaVersion: 1,
+        // Migration
+        migrationBlock: { _ in
+            
+        }
+    )
     
 }
